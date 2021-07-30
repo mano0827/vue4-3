320bb3d36189f8498e0cb6b9bab3a2e5cabea3cd
mano0827 <au.dugout@gmail.com>
2021-07-30 15:52:28 +0900

    vue4-3

--- myvue4-3/src/main.js
+++ myvue4-3/src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAQgibyiIuDXiPJM53RfxYqgxERCWKMvEo",
+  authDomain: "myvue4-2c280.firebaseapp.com",
+  projectId: "myvue4-2c280",
+  storageBucket: "myvue4-2c280.appspot.com",
+  messagingSenderId: "1066329647593",
+  appId: "1:1066329647593:web:869bfd069796be501bc378",
+  measurementId: "G-W0G9NJTSV4"
+
+};
+
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false
 
